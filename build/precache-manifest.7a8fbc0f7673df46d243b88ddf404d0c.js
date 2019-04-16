6f6ed8cac44b68f16619842742435938d5669e3b
cinthyasegura <cinthyaless@gmail.com>
2019-04-16 18:07:34 -0500

    cambios en laster de firsthistory

---
+++ build/precache-manifest.7a8fbc0f7673df46d243b88ddf404d0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "178566866b1c35e1b885",
+    "url": "/static/js/main.b28ecae9.chunk.js"
+  },
+  {
+    "revision": "bf55f937eca7c9949e04",
+    "url": "/static/js/2.bd96124f.chunk.js"
+  },
+  {
+    "revision": "178566866b1c35e1b885",
+    "url": "/static/css/main.c29471fb.chunk.css"
+  },
+  {
+    "revision": "bfa932c77bcd79dc612e3d1a30f98ca0",
+    "url": "/index.html"
+  }
+];
