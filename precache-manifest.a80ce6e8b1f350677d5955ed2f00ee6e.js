5318fea77dd70465629109c50a3a4a9a84ea7460
cinthyasegura <cinthyaless@gmail.com>
2019-04-21 20:12:01 -0500

    Updates

--- precache-manifest.a80ce6e8b1f350677d5955ed2f00ee6e.js
+++ precache-manifest.a80ce6e8b1f350677d5955ed2f00ee6e.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/LIM008-fe-burger-queen/static/js/runtime~main.10776b7b.js"
   },
   {
-    "revision": "e66d8ea43e240245edd0",
+    "revision": "905ee6bccf73f3c37c26",
     "url": "/LIM008-fe-burger-queen/static/js/main.9aae5cea.chunk.js"
   },
   {
@@ -12,11 +12,11 @@ self.__precacheManifest = [
     "url": "/LIM008-fe-burger-queen/static/js/2.0820b56a.chunk.js"
   },
   {
-    "revision": "e66d8ea43e240245edd0",
-    "url": "/LIM008-fe-burger-queen/static/css/main.0b13de7b.chunk.css"
+    "revision": "905ee6bccf73f3c37c26",
+    "url": "/LIM008-fe-burger-queen/static/css/main.b58bc435.chunk.css"
   },
   {
-    "revision": "542d6364e66ae6ede32efa2290284d7d",
+    "revision": "ceba19c51df354753852f94e84a74dfa",
     "url": "/LIM008-fe-burger-queen/index.html"
   }
 ];
